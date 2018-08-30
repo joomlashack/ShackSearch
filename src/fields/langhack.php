39c4d63c1b7c3349ebdbcf8d408ba79ac14ea2cf
Bill Tomczak <bill@ostraining.com>
2018-08-30 16:46:54 -0700

    Standardize file headers

--- src/fields/langhack.php
+++ src/fields/langhack.php
@@ -1,7 +1,26 @@
 <?php
 /**
- * @copyright	Copyright ©2018 Joomlashack. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   ShackSearch
+ * @author    Johan Sundell <labs@pixpro.net>
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright Copyright © You Rock AB 2003-2017 All Rights Reserved.
+ * @copyright 2018 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of ShackSearch.
+ *
+ * ShackSearch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ShackSearch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShackSearch.  If not, see <http://www.gnu.org/licenses/>.
  */
 defined('JPATH_PLATFORM') or die;
 JFormHelper::loadFieldClass('hidden');
@@ -9,19 +28,19 @@ JFormHelper::loadFieldClass('hidden');
 class JFormFieldLangHack extends JFormFieldHidden
 {
 	protected $type = 'LangHack';
-	
+
 	public function __construct($form = null)
 	{
 		parent::__construct($form);
-		
+
 		JFactory::getLanguage()->load( 'com_search' );
 	}
-	
+
 	protected function getInput()
 	{
 		return null;
 	}
-	
+
 	public function __get($name)
 	{
 		return null;
--- src/helper.php
+++ src/helper.php
@@ -1,8 +1,27 @@
 <?php
 /**
-* @copyright	Copyright ©2015 You Rock AB. All rights reserved.
-* @license		GNU General Public License version 2 or later; see LICENSE.txt
-*/
+ * @package   ShackSearch
+ * @author    Johan Sundell <labs@pixpro.net>
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright Copyright © You Rock AB 2003-2017 All Rights Reserved.
+ * @copyright 2018 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of ShackSearch.
+ *
+ * ShackSearch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ShackSearch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShackSearch.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 defined('_JEXEC') or die;
 
@@ -12,13 +31,13 @@ class modShackSearchHelper
 	{
 		return $params;
 	}
-	
+
 	static function init( $params, $id )
 	{
 		$document = JFactory::getDocument();
 		$document->addStyleSheet( '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
 		$document->addStyleSheet( JURI::base().'modules/mod_shacksearch/media/css/mod_shacksearch.css' );
-	
+
 		$settings = new stdClass();
 		$settings->searchText = JText::_( 'MOD_SHACKSEARCH_SEARCH_LABEL' );
 		$settings->nextLinkText = JText::_( 'MOD_SHACKSEARCH_NEXT_LABEL' );
@@ -27,7 +46,7 @@ class modShackSearchHelper
 		$settings->resultText = JText::_( 'MOD_SHACKSEARCH_RESULTS_LABEL' );
 		$settings->readMoreText = JText::_( 'MOD_SHACKSEARCH_READ_MORE_LABEL' );
 		$settings->baseUrl = JURI::root();
-		
+
 		$settings->ordering = $params->get( 'ordering', 'newest' );
 		$settings->use_grouping = (boolean)$params->get( 'use_grouping', false );
 		$settings->searchType = $params->get( 'searchphrase', 'all' );
@@ -37,18 +56,18 @@ class modShackSearchHelper
 		$settings->show_read_more = (boolean)$params->get( 'show_readmore', true );
 		$settings->areas = $params->get( 'areas', array() );
 		$settings->link_read_more = JRoute::_( 'index.php?option=com_search' );
-	
+
 		$document->addScriptDeclaration( 'var ps_settings_'.$id.' = '.json_encode( $settings ).';' );
 		$document->addScript(JURI::root().'modules/mod_shacksearch/media/js/shacksearch.js' );
 		$document->addScriptDeclaration( 'shacksearches.push( '.$id.');' );
 		$document->addScript( JURI::root().'modules/mod_shacksearch/media/js/gshacksearch/gshacksearch.nocache.js' );
 	}
-	
+
 	public static function getAjax()
 	{
 		require_once JPATH_SITE.'/components/com_search/models/search.php';
 		require_once JPATH_ADMINISTRATOR.'/components/com_search/helpers/search.php';
-		
+
 		$input = JFactory::getApplication()->input;
 		$word = $input->getString( 'searchword', '' );
 		$ordering = $input->getString( 'ordering', 'newest' );
@@ -60,10 +79,10 @@ class modShackSearchHelper
         if( $word != '' )
 		{
 			$model = new SearchModelSearch();
-			
+
 			// log the search
 			SearchHelper::logSearch($word);
-			
+
 			$lang = JFactory::getLanguage();
 			$upper_limit = $lang->getUpperLimitSearchWord();
 			$lower_limit = $lang->getLowerLimitSearchWord();
@@ -71,26 +90,26 @@ class modShackSearchHelper
 			{
 				$error = JText::sprintf('COM_SEARCH_ERROR_SEARCH_MESSAGE', $lower_limit, $upper_limit);
 			}
-			
+
 			if (SearchHelper::santiseSearchWord($word, $model->getState()->get( 'match' ) ) )
 			{
 				$error = JText::_('COM_SEARCH_ERROR_IGNOREKEYWORD');
 			}
-			
+
 			$model->getState()->set('keyword', $word);
-			
+
 			if( $error == null )
 			{
 				$model->setSearch( $word, $searchphrase, $ordering );
 				$results = $model->getData();
 				$total = $model->getTotal();
-				
+
 				require_once JPATH_SITE . '/components/com_content/helpers/route.php';
-				
+
 				for ($i=0, $count = count($results); $i < $count; $i++)
 				{
 					$row = &$results[$i]->text;
-						
+
 					if ($model->getState()->get('match') == 'exact')
 					{
 						$searchwords = array($word);
@@ -102,12 +121,12 @@ class modShackSearchHelper
 						$searchwords = preg_split("/\s+/u", $searchworda);
 						$needle = $searchwords[0];
 					}
-						
+
 					$row = SearchHelper::prepareSearchContent($row, $needle);
 					$searchwords = array_unique($searchwords);
 					$searchRegex = '#(';
 					$x = 0;
-						
+
 					foreach ($searchwords as $k => $hlword)
 					{
 						$searchRegex .= ($x == 0 ? '' : '|');
@@ -115,9 +134,9 @@ class modShackSearchHelper
 						$x++;
 					}
 					$searchRegex .= ')#iu';
-						
+
 					$row = preg_replace($searchRegex, '<span class="highlight">\0</span>', $row);
-						
+
 					$result = &$results[$i];
 					if ($result->created)
 					{
@@ -127,7 +146,7 @@ class modShackSearchHelper
 					{
 						$created = '';
 					}
-						
+
 					$result->text		= JHtml::_('content.prepare', $result->text, '', 'com_search.search');
 					$result->created	= $created;
 					$result->count		= $i + 1;
@@ -137,7 +156,7 @@ class modShackSearchHelper
 		}
 		$searchResult->items = $results;
 		$searchResult->total = $total;
-		
+
 		return '('.json_encode( $searchResult ).');';
 	}
 }
